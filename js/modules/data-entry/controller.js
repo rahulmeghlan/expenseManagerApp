6d27e2c787e59ae1d8b0c85e132fe01a95c28e25
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-05-18 22:27:56 +0530

    fixed the select box issues and the logical issue of currency and types

--- js/modules/data-entry/controller.js
+++ js/modules/data-entry/controller.js
@@ -5,33 +5,56 @@ expenseMgr.controller('dataEntry', ['$scope', function ($scope) {
     $scope.expenseMsg = "Add a new expense";
     $scope.formHeader = "Add a new item";
     $scope.isFrOpen = false;
+
+    $scope.paymentTypes = ["Card", "Cash", "Other"];
+    $scope.currencies = ["USD", "INR", "Euro"];
+
+
+    $scope.newItem = {
+        name: "",
+        type: "Other",
+        spentOn: "test",
+        currency: "Euro",
+        date: "2013/08/01",
+        amount: 0
+    };
     $scope.items = [
         {
             name: "Andy",
-            type: "card",
+            type: "Card",
             spentOn: "Grocery",
             currency: "USD",
-            date: "01/08/2013",
+            date: "2013/08/01",
             amount: 10
         },
         {
             name: "Candy",
-            type: "cash",
+            type: "Cash",
             spentOn: "Bar",
             currency: "INR",
-            date: "01/08/2013",
+            date: "2013/08/01",
             amount: 50
         }
     ];
-    $scope.selectedIndex = 0;
+    $scope.selectedIndex = 1;
     $scope.selectedItem = $scope.items[$scope.selectedIndex];
 
     $scope.changeFriend = function () {
         $scope.selectedItem = $scope.items[parseInt(this.selectedIndex)];
     };
 
-    $scope.addFriend = function(){
-      if($scope.isFrOpen) $scope.isFrOpen = false;
-        else $scope.isFrOpen = true;
+    $scope.addFriend = function () {
+        if ($scope.isFrOpen) {
+            $scope.items.push(angular.copy($scope.newItem));
+            var lastItem = $scope.items.length-1;
+            $scope.selectedIndex = lastItem;
+            $scope.selectedItem = $scope.items[lastItem];
+            $scope.newItem.name = "";
+            $scope.isFrOpen = false;
+            console.log(">>>>>>>", $scope.items);
+        }
+        else {
+            $scope.isFrOpen = true;
+        }
     };
 }]);
