c07d522691465b2aeeb2533a6a9c92133f81ab4b
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-05-18 23:38:47 +0530

    interim commit

--- js/modules/data-entry/controller.js
+++ js/modules/data-entry/controller.js
@@ -1,43 +1,50 @@
 // This is the controller to handle the dataEntry operations
 
 //todo : need to scope the expenseMgr
-expenseMgr.controller('dataEntry', ['$scope', function ($scope) {
-    $scope.expenseMsg = "Add a new expense";
-    $scope.formHeader = "Add a new item";
-    $scope.isFrOpen = false;
+expenseMgr.controller('dataEntry', ['$scope', 'ExpenseMgrService', function ($scope, expenseMgrService) {
 
-    $scope.paymentTypes = ["Card", "Cash", "Other"];
-    $scope.currencies = ["USD", "INR", "Euro"];
+    // This function is used to initialize the app
+    var init = function () {
+        $scope.expenseMsg = "Add a new expense";
+        $scope.formHeader = "Add a new item";
+        $scope.isFrOpen = false;
+
+        $scope.paymentTypes = ["Card", "Cash", "Other"];
+        $scope.currencies = ["USD", "INR", "Euro"];
 
 
-    $scope.newItem = {
-        name: "",
-        type: "Other",
-        spentOn: "test",
-        currency: "Euro",
-        date: "2013/08/01",
-        amount: 0
+        $scope.newItem = {
+            name: "",
+            type: "Other",
+            spentOn: "test",
+            currency: "Euro",
+            date: "2013/08/01",
+            amount: 0
+        };
+        $scope.items = [
+            {
+                name: "Andy",
+                type: "Card",
+                spentOn: "Grocery",
+                currency: "USD",
+                date: "2013/08/01",
+                amount: 10
+            },
+            {
+                name: "Candy",
+                type: "Cash",
+                spentOn: "Bar",
+                currency: "INR",
+                date: "2013/08/01",
+                amount: 50
+            }
+        ];
+        $scope.selectedIndex = 1;
+        $scope.selectedItem = $scope.items[$scope.selectedIndex];
+        expenseMgrService.items = $scope.items;
+        expenseMgrService.currencies = $scope.currencies;
     };
-    $scope.items = [
-        {
-            name: "Andy",
-            type: "Card",
-            spentOn: "Grocery",
-            currency: "USD",
-            date: "2013/08/01",
-            amount: 10
-        },
-        {
-            name: "Candy",
-            type: "Cash",
-            spentOn: "Bar",
-            currency: "INR",
-            date: "2013/08/01",
-            amount: 50
-        }
-    ];
-    $scope.selectedIndex = 1;
-    $scope.selectedItem = $scope.items[$scope.selectedIndex];
+
 
     $scope.changeFriend = function () {
         $scope.selectedItem = $scope.items[parseInt(this.selectedIndex)];
@@ -46,15 +53,21 @@ expenseMgr.controller('dataEntry', ['$scope', function ($scope) {
     $scope.addFriend = function () {
         if ($scope.isFrOpen) {
             $scope.items.push(angular.copy($scope.newItem));
-            var lastItem = $scope.items.length-1;
+            var lastItem = $scope.items.length - 1;
             $scope.selectedIndex = lastItem;
             $scope.selectedItem = $scope.items[lastItem];
             $scope.newItem.name = "";
             $scope.isFrOpen = false;
-            console.log(">>>>>>>", $scope.items);
         }
         else {
             $scope.isFrOpen = true;
         }
     };
+
+    $scope.saveExpense = function () {
+        expenseMgrService.items = $scope.items;
+        $scope.$emit("expenseUpdated");
+    };
+
+    init();
 }]);
--- js/modules/data-view/controller.js
+++ js/modules/data-view/controller.js
@@ -1,3 +1,19 @@
-expenseMgr.controller('dataView', ['$scope', function ($scope) {
-    $scope.greetMe = 'World';
+expenseMgr.controller('dataView', ['$rootScope', '$scope', 'ExpenseMgrService', function ($rootScope, $scope, expenseMgrService) {
+
+    var init = function () {
+//        $scope.items = ["all"];
+//        $scope.items.push(expenseMgrService.items); // initialise the items based on the default list of data-entry
+        $scope.currencies = expenseMgrService.currencies; // initialize the currencies based on the default list of data-entry
+
+        $scope.selectedCurrency = $scope.currencies[0];// set the initial value of currencies
+    };
+
+    var bindEvents = function () {
+        $rootScope.$on("expenseUpdated", function () {
+            $scope.items = expenseMgrService.items;
+        })
+    };
+
+    bindEvents();
+    init();
 }]);
